a53a3328f6279a8d18570091ce9d90fa14df2182
Sienna94 <csj19940407@gmail.com>
2020-12-30 14:22:39 +0900

    JSP 서버 연동 로그인

--- app/src/main/java/com/example/again/MainActivity.java
+++ app/src/main/java/com/example/again/MainActivity.java
@@ -4,12 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView img;
     TextView tv;
     TextView tv2;
     EditText etUser;
@@ -22,15 +39,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        img=findViewById(R.id.iv_title);
         tv=findViewById(R.id.tv_user);
         tv2=findViewById(R.id.tv_pw);
         etUser = findViewById(R.id.et_user);
         etPw = findViewById(R.id.et_pw);
         btJoin = findViewById(R.id.btn_join);
         btLogin=findViewById(R.id.btn_login);
-
         btJoin.setOnClickListener(this);
         btLogin.setOnClickListener(this);
+    }
+    //ID PW 유효성 검사
+    private boolean isValid() {
+        boolean isValid = true;
+        if (etUser.getText().toString().trim().length() < 1) {
+            isValid = false;
+        } else if (etUser.getText().toString().trim().contains(" ")) {
+            isValid = false;
+        } else if (etPw.getText().toString().trim().contains(" ")) {
+            isValid = false;
+        } else if (etPw.getText().toString().trim().length() < 1) {
+            isValid = false;
+        }
+        return isValid;
     }
 
     @Override
@@ -47,14 +78,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onDestroy() {
         super.onDestroy();
     }
+    Response.ErrorListener errorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d("kkk", "로그인 실패");
+        }
+    };
 
+    Response.Listener<String> successListener = new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+            Log.d("kkk", response);
+
+        }
+    };
     @Override
     public void onClick(View v) {
         if(v.getId()==R.id.btn_join){
             Intent intent = new Intent(this, com.example.again.JoinActivity.class);
             startActivity(intent);
         }else if(v.getId()==R.id.btn_login){
+            //로그인 버튼 누르면 1. 통신 2. 값 받아오기 3. alert 띄우기 4. 리스트뷰로 이동
+            //로그인 시도
+            final String id = etUser.getText().toString().trim();
+            final String pw = etPw.getText().toString().trim();
+            //post//
+            RequestQueue stringRequest = Volley.newRequestQueue(this);
+            String url = "http://192.168.7.26:8180/oop/androidLogin.do";
 
+            StringRequest myReq = new StringRequest(Request.Method.POST, url,
+                    successListener, errorListener) {
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("id",id);
+                    params.put("pass", pw);
+                    return params;
+                }
+            };
+            myReq.setRetryPolicy(new DefaultRetryPolicy(3000, 0, 1f)
+            );
+            stringRequest.add(myReq);
+        } else {
+            Toast.makeText(this, "아이디 패스워드를 확인하세요 :(", Toast.LENGTH_SHORT).show();
         }
     }
 }
--- app/src/main/java/com/example/again/MyListActivity.java
+++ app/src/main/java/com/example/again/MyListActivity.java
@@ -3,12 +3,30 @@ package com.example.again;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MyListActivity extends AppCompatActivity {
+/*    1. item 위한 레이아웃 만들기
+      2. item 위한 데이터클래스 만들기 / item xml
 
+      3. 어레이 리스트 만들기
+      5. ListView 만들기
+      6. 아답터 클래스 복붙하기
+      7. 아답터 빨간 부분 수정(대부분 대문자는 import 하면 됨 holder 제외)
+      8. 이전과 동일*/
+    ArrayList<ItemData> arr = new ArrayList<>();
+
+    ListView lvMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_list);
+        lvMain = findViewById(R.id.lv1);
+    }
+
+    class ItemHolder{
+
     }
 }
